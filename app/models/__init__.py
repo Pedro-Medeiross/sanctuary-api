b7d6a1168c90a41beeee4acb43ba1de967736923
Pedro Medeiros <pedro.contato166@hotmail.com>
2026-04-28 19:54:37 -0300

    Updating models init.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,7 @@
+# app/models/__init__.py
+from app.models.user import User
+from app.models.role import Role, user_roles
+from app.models.user_connection import UserConnection
+from app.models.session import Session
 from app.models.guild import Guild
 from app.models.log_channel import LogChannel
-from app.models.user import User
-from app.models.session import Session
